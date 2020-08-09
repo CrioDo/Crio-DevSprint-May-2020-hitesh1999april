f4d009f104802a0f6ba42b2901412f9fae46afa2
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-08-09 13:18:53 +0530

    Updates

---
+++ precache-manifest.333f11bf6aa8250c3076e4218c0b49f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf3ec0f47e0b158014f8239a1a18086",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/index.html"
+  },
+  {
+    "revision": "148eee445a204ac465c9",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "5a3a44b64bf4e3afd711",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "148eee445a204ac465c9",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/js/2.4ca311fd.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/js/2.4ca311fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3a44b64bf4e3afd711",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/js/main.252dd80e.chunk.js"
+  },
+  {
+    "revision": "f3b6b8f518d9e0516c94",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/js/runtime-main.7d057f2b.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-DevSprint-May-2020-hitesh1999april/static/media/crio-logo.63be71d0.svg"
+  }
+]);
